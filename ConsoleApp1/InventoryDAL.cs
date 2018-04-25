8a251df485f6b95ea7fffd5b425bff446bf66c46
nguyentoan1102 <longtinh12@gmail.com>
2018-04-26 01:13:35 +0700

    Example ADO.NET Working with StoreProceduce

---
+++ ConsoleApp1/InventoryDAL.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class InventoryDAL
+    {
+        // This member will be used by all methods.
+        private SqlConnection sqlCn = null;
+
+        public void OpenConnection(string connectionString)
+        {
+            sqlCn = new SqlConnection();
+            sqlCn.ConnectionString = connectionString;
+            sqlCn.Open();
+        }
+
+        public void InsertAuto(NewCar newCar)
+        {
+            // Format and execute SQL statement.
+            string sql = string.Format("Insert Into Inventory" +
+            "( Make, Color, PetName) Values" +
+            "('{0}', '{1}', '{2}')", newCar.Make, newCar.Color, newCar.PetName);
+            // Execute using our connection.
+            using (SqlCommand cmd = new SqlCommand(sql, sqlCn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public string LookUpPetName(int carID)
+        {
+            string carPetName = string.Empty;
+            //establish name of stored proc.
+            using (SqlCommand cmd = new SqlCommand("GetPetName", sqlCn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                // Input param.
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@carID";
+                param.SqlDbType = SqlDbType.Int;
+                param.Value = carID;
+                // The default direction is in fact Input, but to be clear:
+                param.Direction = ParameterDirection.Input;
+                cmd.Parameters.Add(param);
+                // Output param.
+                param = new SqlParameter();
+                param.ParameterName = "@petName";
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                param.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(param);
+                param = new SqlParameter();
+                param.ParameterName = "@color";
+                param.SqlDbType = SqlDbType.VarChar;
+                param.Size = 10;
+                param.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(param);
+
+                // Execute the stored proc.
+                cmd.ExecuteNonQuery();
+                // Return output param.
+                carPetName = (string)cmd.Parameters["@petName"].Value + "\t Car's Color: " + (string)cmd.Parameters["@color"].Value;
+            }
+            return carPetName;
+        }
+
+        public void CloseConnection()
+        {
+            sqlCn.Close();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string dp = ConfigurationManager.AppSettings["provider"];
+            string connectionString = ConfigurationManager.AppSettings["cnStr"];
+            //Get the factory provider.
+            DbProviderFactory df = DbProviderFactories.GetFactory(dp);
+            //Now get the connection object.
+            // Read data from database with CommandType.Text
+            //ReadDataWithDataReader(dp, connectionString, df);
+            //Get pet name with storeproceduce
+            InventoryDAL invenCar = new InventoryDAL();
+            invenCar.OpenConnection(connectionString);
+            Console.WriteLine("Car's PetName is:" + invenCar.LookUpPetName(5));
+            invenCar.CloseConnection();
+            Console.ReadLine();
+        }
+
+        private static void ReadDataWithSqlCommand()
+        {
+        }
+
+        private static void ReadDataWithDataReader(string dataProvider, string connectionString, DbProviderFactory df)
+        {
+            using (DbConnection cn = df.CreateConnection())
+            {
+                Console.WriteLine("Your connection object is a: {0}", cn.GetType().Name);
+                cn.ConnectionString = connectionString;
+                cn.Open();
+                // Make command object.
+                DbCommand cmd = df.CreateCommand();
+                Console.WriteLine("Your command object is a : {0}", cmd.GetType().Name);
+                cmd.Connection = cn;
+                cmd.CommandText = "Select * From Inventory";
+
+                // Print out data with data reader.
+                using (DbDataReader dr = cmd.ExecuteReader())
+                {
+                    Console.WriteLine("Your data reader object is a: {0}", dr.GetType().Name);
+                    Console.WriteLine("\n***** Current Inventory *****");
+                    while (dr.Read())
+                        Console.WriteLine("-> Car #{0} is a {1}.",
+                        dr["CarID"], dr["Make"].ToString());
+                }
+            }
+        }
+    }
+}
